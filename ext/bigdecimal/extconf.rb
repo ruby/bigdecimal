57dd1b0b70abbc051833a9d4dbf4edf7cb2dd4ba
Kenta Murata <mrkn@mrkn.jp>
2016-10-11 16:40:11 +0900

    Fix for ruby < 2.4

--- ext/bigdecimal/extconf.rb
+++ ext/bigdecimal/extconf.rb
@@ -16,3 +16,12 @@ have_macro("RARRAY_CONST_PTR", "ruby.h")
 have_macro("RARRAY_AREF", "ruby.h")
 
 create_makefile('bigdecimal')
+
+# Add additional dependencies
+open('Makefile', 'a') do |io|
+  if RUBY_VERSION >= '2.4'
+    io.puts <<-MAKEFILE
+bigdecimal.o: $(hdrdir)/ruby/backward.h
+    MAKEFILE
+  end
+end
