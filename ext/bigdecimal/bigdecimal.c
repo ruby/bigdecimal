ruby/bigdecimal

commit 3081a627cebdc1fc119425c7a9f009dbb6bec8e8

Raise error for an invalid string for BigDecimal()
This fixes [Bug #10286](https://bugs.ruby-lang.org/issues/10286).

Note that this commit introduces an incompatibility that a string
beginning with `_` like `_1_1_1` is not valid.

ext/bigdecimal/bigdecimal.c
@@ -3967,7 +3967,8 @@ overflow:
 VP_EXPORT Real *
 VpAlloc(size_t mx, const char *szVal)
 {
-    size_t i, ni, ipn, ipf, nf, ipe, ne, nalloc;
+    const char *orig_szVal = szVal;
+    size_t i, ni, ipn, ipf, nf, ipe, ne, dot_seen, exp_seen, nalloc;
     char v, *psz;
     int  sign=1;
     Real *vp = NULL;
@@ -4062,9 +4063,12 @@ VpAlloc(size_t mx, const char *szVal)
     ipf = 0;
     ipe = 0;
     ne  = 0;
+    dot_seen = 0;
+    exp_seen = 0;
     if (v) {
         /* other than digit nor \0 */
         if (szVal[i] == '.') {    /* xxx. */
+            dot_seen = 1;
             ++i;
             ipf = i;
             while ((v = szVal[i]) != 0) {    /* get fraction part. */
@@ -4080,6 +4084,7 @@ VpAlloc(size_t mx, const char *szVal)
                 break;
             case 'e': case 'E':
             case 'd': case 'D':
+                exp_seen = 1;
                 ++i;
                 ipe = i;
                 v = szVal[i];
@@ -4094,6 +4099,11 @@ VpAlloc(size_t mx, const char *szVal)
                 break;
         }
     }
+    if (((ni == 0 || dot_seen) && nf == 0) || (exp_seen && ne == 0)) {
+	VALUE str = rb_str_new2(orig_szVal);
+	rb_raise(rb_eArgError, "invalid value for BigDecimal(): \"%"PRIsVALUE"\"", str);
+    }
+
     nalloc = (ni + nf + BASE_FIG - 1) / BASE_FIG + 1;    /* set effective allocation  */
     /* units for szVal[]  */
     if (mx == 0) mx = 1;

test/test_bigdecimal.rb
@@ -55,6 +55,17 @@ class TestBigDecimal < Test::Unit::TestCase
     assert_raise(ArgumentError) { BigDecimal("1", -1) }
   end
 
+  def test_global_new_with_invalid_string
+    [
+      '', '.', 'e1', 'd1', '.e', '.d', '1.e', '1.d', '.1e', '.1d',
+      'invlaid value'
+    ].each do |invalid_string|
+      assert_raise_with_message(ArgumentError, %Q[invalid value for BigDecimal(): "#{invalid_string}"]) do
+        BigDecimal(invalid_string)
+      end
+    end
+  end
+
   def test_global_new_with_integer
     assert_equal(BigDecimal("1"), BigDecimal(1))
     assert_equal(BigDecimal("-1"), BigDecimal(-1))
@@ -114,8 +125,9 @@ class TestBigDecimal < Test::Unit::TestCase
     assert_equal(1, BigDecimal.new("1", 1))
     assert_equal(1, BigDecimal.new(" 1 "))
     assert_equal(111, BigDecimal.new("1_1_1_"))
-    assert_equal(0, BigDecimal.new("_1_1_1"))
     assert_equal(10**(-1), BigDecimal.new("1E-1"), '#4825')
+
+    assert_raise(ArgumentError, /"_1_1_1"/) { BigDecimal.new("_1_1_1") }
 
     BigDecimal.mode(BigDecimal::EXCEPTION_OVERFLOW, false)
     BigDecimal.mode(BigDecimal::EXCEPTION_NaN, false)

commit 0aa8f96519e1c61913ba45b05e2188c036ac1f01

when parsing strings, allow a trailing decimal point (fixes a problem introduced in 3081a627cebdc1fc119425c7a9f009dbb6bec8e8)

ext/bigdecimal/bigdecimal.c
@@ -4141,12 +4141,10 @@ VpAlloc(size_t mx, const char *szVal)
     ipf = 0;
     ipe = 0;
     ne  = 0;
-    dot_seen = 0;
     exp_seen = 0;
     if (v) {
         /* other than digit nor \0 */
         if (szVal[i] == '.') {    /* xxx. */
-            dot_seen = 1;
             ++i;
             ipf = i;
             while ((v = szVal[i]) != 0) {    /* get fraction part. */
@@ -4177,7 +4175,7 @@ VpAlloc(size_t mx, const char *szVal)
                 break;
         }
     }
-    if (((ni == 0 || dot_seen) && nf == 0) || (exp_seen && ne == 0)) {
+    if ((ni == 0 && nf == 0) || (exp_seen && ne == 0)) {
 	VALUE str = rb_str_new2(orig_szVal);
 	rb_raise(rb_eArgError, "invalid value for BigDecimal(): \"%"PRIsVALUE"\"", str);
     }

test/test_bigdecimal.rb
@@ -65,7 +65,7 @@ class TestBigDecimal < Test::Unit::TestCase
   def test_global_new_with_invalid_string
     [
       '', '.', 'e1', 'd1', '.e', '.d', '1.e', '1.d', '.1e', '.1d',
-      'invlaid value'
+      'invalid value'
     ].each do |invalid_string|
       assert_raise_with_message(ArgumentError, %Q[invalid value for BigDecimal(): "#{invalid_string}"]) do
         BigDecimal(invalid_string)
@@ -78,6 +78,7 @@ class TestBigDecimal < Test::Unit::TestCase
     assert_equal(BigDecimal("-1"), BigDecimal(-1))
     assert_equal(BigDecimal((2**100).to_s), BigDecimal(2**100))
     assert_equal(BigDecimal((-2**100).to_s), BigDecimal(-2**100))
+    assert_equal(BigDecimal("123."), BigDecimal(123))
   end
 
   def test_global_new_with_rational
@@ -88,7 +89,9 @@ class TestBigDecimal < Test::Unit::TestCase
 
   def test_global_new_with_float
     assert_equal(BigDecimal("0.1235"), BigDecimal(0.1234567, 4))
+    assert_equal(BigDecimal(".1235"), BigDecimal(0.1234567, 4))
     assert_equal(BigDecimal("-0.1235"), BigDecimal(-0.1234567, 4))
+    assert_equal(BigDecimal("-.1235"), BigDecimal(-0.1234567, 4))
     assert_raise_with_message(ArgumentError, "can't omit precision for a Float.") { BigDecimal(4.2) }
     assert_raise(ArgumentError) { BigDecimal(0.1, Float::DIG + 2) }
     assert_nothing_raised { BigDecimal(0.1, Float::DIG + 1) }