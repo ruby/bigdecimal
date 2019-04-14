c0210eb339ff6407de2737e29198a7ec7cb26902
Kenta Murata <mrkn@mrkn.jp>
2019-04-14 10:32:08 +0900

    Fix for rake-compiler-dock

--- ext/bigdecimal/util/extconf.rb
+++ ext/bigdecimal/util/extconf.rb
@@ -14,7 +14,13 @@ checking_for(checking_message("bigdecimal.so")) do
     tmp_build_base_dir = Pathname("tmp")/RUBY_PLATFORM/"bigdecimal"
     if current_dir == tmp_build_base_dir/"util"/RUBY_VERSION
       lib_dir = base_dir/"lib"
-      bigdecimal_so = lib_dir/"bigdecimal.#{RbConfig::CONFIG['DLEXT']}"
+      if RUBY_PLATFORM =~ /cygwin|mingw/
+        # For rake-compiler-dock
+        ver = RUBY_VERSION.split('.')[0, 2].join('.')
+        bigdecimal_so = lib_dir/ver/"bigdecimal.#{RbConfig::CONFIG['DLEXT']}"
+      else
+        bigdecimal_so = lib_dir/"bigdecimal.#{RbConfig::CONFIG['DLEXT']}"
+      end
       break false unless bigdecimal_so.exist?
       $libs << " #{bigdecimal_so}"
     else
