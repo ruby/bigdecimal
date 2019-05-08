c774244918780138cb46fa5cc0129f9ada20e31f
Kenta Murata <mrkn@mrkn.jp>
2019-05-08 18:13:53 +0900

    Fix for Windows

--- ext/bigdecimal/util/extconf.rb
+++ ext/bigdecimal/util/extconf.rb
@@ -2,32 +2,40 @@
 require 'mkmf'
 require 'pathname'
 
-checking_for(checking_message("bigdecimal.so")) do
-  if RUBY_PLATFORM =~ /mswin/
-    $DLDFLAGS << " -libpath:.."
-    $libs << " bigdecimal-$(arch).lib"
-    true
-  else
-    base_dir = Pathname("../../../..").expand_path(__FILE__)
-    current_dir = Pathname.pwd.relative_path_from(base_dir)
-
-    tmp_build_base_dir = Pathname("tmp")/RUBY_PLATFORM/"bigdecimal"
-    if current_dir == tmp_build_base_dir/"util"/RUBY_VERSION
-      lib_dir = base_dir/"lib"
-      if RUBY_PLATFORM =~ /cygwin|mingw/
-        # For rake-compiler-dock
-        ver = RUBY_VERSION.split('.')[0, 2].join('.')
-        bigdecimal_so = lib_dir/ver/"bigdecimal.#{RbConfig::CONFIG['DLEXT']}"
-      else
-        bigdecimal_so = lib_dir/"bigdecimal.#{RbConfig::CONFIG['DLEXT']}"
-      end
-      break false unless bigdecimal_so.exist?
-      $libs << " #{bigdecimal_so}"
+def check_bigdecimal_so
+  checking_for(checking_message("bigdecimal.so")) do
+    if RUBY_PLATFORM =~ /mswin/
+      $DLDFLAGS << " -libpath:.."
+      $libs << " bigdecimal-$(arch).lib"
+      true
     else
-      $libs << " $(TARGET_SO_DIR)../bigdecimal.so"
+      base_dir = Pathname("../../../..").expand_path(__FILE__)
+      current_dir = Pathname.pwd.relative_path_from(base_dir)
+
+      tmp_build_base_dir = Pathname("tmp")/RUBY_PLATFORM/"bigdecimal"
+      if current_dir == tmp_build_base_dir/"util"/RUBY_VERSION
+        lib_dir = base_dir/"lib"
+        bigdecimal_so = lib_dir/"bigdecimal.#{RbConfig::CONFIG['DLEXT']}"
+        unless bigdecimal_so.exist?
+          if RUBY_PLATFORM =~ /cygwin|mingw/
+            # For rake-compiler-dock
+            ver = RUBY_VERSION.split('.')[0, 2].join('.')
+            bigdecimal_so = lib_dir/ver/"bigdecimal.#{RbConfig::CONFIG['DLEXT']}"
+          end
+          break false unless bigdecimal_so.exist?
+        end
+        $libs << " #{bigdecimal_so}"
+      else
+        $libs << " $(TARGET_SO_DIR)../bigdecimal.so"
+      end
+      true
     end
-    true
   end
+end
+
+unless check_bigdecimal_so
+  $stderr.puts "Unable to find bigdecimal.so"
+  abort
 end
 
 create_makefile('bigdecimal/util')
