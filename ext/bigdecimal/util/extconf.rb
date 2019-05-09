16884e2349cb6adca27f2b773272584e2900c116
Kenta Murata <mrkn@users.noreply.github.com>
2019-05-10 00:02:51 +0900

    Merge pull request #142 from ruby/fix_for_gem_install
    Fix for gem install

--- ext/bigdecimal/util/extconf.rb
+++ ext/bigdecimal/util/extconf.rb
@@ -12,22 +12,20 @@ def check_bigdecimal_so
       base_dir = Pathname("../../../..").expand_path(__FILE__)
       current_dir = Pathname.pwd.relative_path_from(base_dir)
 
-      tmp_build_base_dir = Pathname("tmp")/RUBY_PLATFORM/"bigdecimal"
-      if current_dir == tmp_build_base_dir/"util"/RUBY_VERSION
-        lib_dir = base_dir/"lib"
-        bigdecimal_so = lib_dir/"bigdecimal.#{RbConfig::CONFIG['DLEXT']}"
-        unless bigdecimal_so.exist?
-          if RUBY_PLATFORM =~ /cygwin|mingw/
+      lib_dir = base_dir/"lib"
+      bigdecimal_so = lib_dir/"bigdecimal.#{RbConfig::CONFIG['DLEXT']}"
+      unless bigdecimal_so.exist?
+        if RUBY_PLATFORM =~ /cygwin|mingw/
+          tmp_build_base_dir = Pathname("tmp")/RUBY_PLATFORM/"bigdecimal"
+          if current_dir == tmp_build_base_dir/"util"/RUBY_VERSION
             # For rake-compiler-dock
             ver = RUBY_VERSION.split('.')[0, 2].join('.')
             bigdecimal_so = lib_dir/ver/"bigdecimal.#{RbConfig::CONFIG['DLEXT']}"
           end
-          break false unless bigdecimal_so.exist?
         end
-        $libs << " #{bigdecimal_so}"
-      else
-        $libs << " $(TARGET_SO_DIR)../bigdecimal.so"
+        break false unless bigdecimal_so.exist?
       end
+      $libs << " #{bigdecimal_so}"
       true
     end
   end
